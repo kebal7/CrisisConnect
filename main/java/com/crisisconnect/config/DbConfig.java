c1a67f7aac5589f1c0d82db0ae832f4d9852d00b
Kebal Badal <kebal.badal555@gmail.com>
2025-04-12 23:26:10 +0545

    creating DbConfig.java

---
+++ main/java/com/crisisconnect/config/DbConfig.java
@@ -0,0 +1,18 @@
+package com.crisisconnect.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConfig {
+	private static final String DB_NAME = "CrisisConnect";
+	private static final String URL = "jdbc:mysql://localhost:3306/" + DB_NAME;
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "";
+	
+	public static Connection getDbConnection() throws SQLException, ClassNotFoundException{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+	}
+	
+}
