3c82c31c6386e411acd8db0a8bdcde00194caadf
Kebal Badal <kebal.badal555@gmail.com>
2025-04-13 10:22:12 +0545

    login.jsp form data retrieved in login servlet and created login model

--- main/java/com/crisisconnect/controller/servlet/LoginPageController.java
+++ main/java/com/crisisconnect/controller/servlet/LoginPageController.java
@@ -33,7 +33,11 @@ public class LoginPageController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String formUserName = request.getParameter("username");
+		String formPassword = request.getParameter("password");
+		
+		
+		
 		doGet(request, response);
 	}
 
