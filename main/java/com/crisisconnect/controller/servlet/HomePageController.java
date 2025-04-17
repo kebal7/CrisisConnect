aee1f6f638914efc8466e72d883533dc12a0936e
Kebal Badal <kebal.badal555@gmail.com>
2025-04-17 09:37:35 +0545

    added session verification in home page

--- main/java/com/crisisconnect/controller/servlet/HomePageController.java
+++ main/java/com/crisisconnect/controller/servlet/HomePageController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 /**
@@ -26,7 +28,17 @@ public class HomePageController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("WEB-INF/pages/home.jsp").forward(request, response);
+		HttpSession session = request.getSession(false);
+		
+		if(session == null ){
+			response.sendRedirect(request.getContextPath() + "/login");
+		}else if(session.getAttribute("username") == null){
+			response.sendRedirect(request.getContextPath() + "/login");
+		}else if(session.getAttribute("username").equals("")) {
+			response.sendRedirect(request.getContextPath() + "/login");
+		}else {
+			request.getRequestDispatcher("WEB-INF/pages/home.jsp").forward(request, response);
+		}
 	}
 
 	/**
