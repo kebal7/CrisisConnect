8a47518dec3dad792ba1254e8425a8e376a8e9ec
Kebal Badal <kebal.badal555@gmail.com>
2025-04-17 08:41:30 +0545

    added new fields in usermode

--- main/java/com/crisisconnect/model/UserModel.java
+++ main/java/com/crisisconnect/model/UserModel.java
@@ -2,15 +2,27 @@ package com.crisisconnect.model;
 
 public class UserModel {
 	private String username;
-	private String email;
-	private String usertype;
+	private String fullName;
+	private String userType;
 	private String password;
+	private String email;
+	private String phoneNumber;
+	private String dateOfBirth;
+	private String address;
+	private String imagePath;
 	
-	public UserModel(String username, String email, String usertype, String password) {
+	public UserModel(String username, String fullName, String userType, String password, String email,
+			String phoneNumber, String dateOfBirth, String address, String imagePath) {
+		super();
 		this.username = username;
-		this.email = email;
-		this.usertype = usertype;
+		this.fullName = fullName;
+		this.userType = userType;
 		this.password = password;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.dateOfBirth = dateOfBirth;
+		this.address = address;
+		this.imagePath = imagePath;
 	}
 
 	public String getUsername() {
@@ -21,20 +33,20 @@ public class UserModel {
 		this.username = username;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getFullName() {
+		return fullName;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
 	}
 
-	public String getUsertype() {
-		return usertype;
+	public String getUserType() {
+		return userType;
 	}
 
-	public void setUsertype(String usertype) {
-		this.usertype = usertype;
+	public void setUserType(String userType) {
+		this.userType = userType;
 	}
 
 	public String getPassword() {
@@ -44,6 +56,47 @@ public class UserModel {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(String dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	
 	
 	
 }
